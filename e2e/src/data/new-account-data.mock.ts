f3d5203771c69f558c42c1e3d31c177a8b219b90
KatyaFedorova <legion.is.my.name1984@gmail.com>
2020-02-09 20:09:28 +0200

    Finish refactoring related to site redesign

--- e2e/src/data/new-account-data.mock.ts
+++
@@ -1,9 +0,0 @@
-import { getRandom } from '../helper/utils';
-
-export const NewAccountDataMock = {
-  email: `valor+${ getRandom() }@gmail.com`,
-  password: 'valor1@',
-  firstName: 'Ivan',
-  lastName: 'Ivanov',
-  userId: `valor${ getRandom() }`
-};
--- e2e/src/support/search-project.po.ts
+++
@@ -1,9 +0,0 @@
-import {$} from 'protractor';
-import { BaseComponent } from '../helper/base.component';
-
-export class SearchProjectPo extends BaseComponent {
-  url = '/search/projects';
-
-  textUserName = $('fl-text.Username-displayName');
-  textUserId = $('fl-text.Username-userId');
-}
