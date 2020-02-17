e0fd31001e2d799832daec6d1744013ea8cad750
KatyaFedorova <legion.is.my.name1984@gmail.com>
2020-02-17 07:19:45 +0200

    WIP

--- e2e/src/pages/login.po.ts
+++ e2e/src/pages/login.po.ts
@@ -7,6 +7,17 @@ export class LoginPo extends BaseComponent {
   readonly inputUserName = $('#username');
   readonly inputPassword = $('#password');
   readonly btnLogin = $('#login_btn');
+  readonly textPswrdErrorMessage = $('.alert-error span');
+  readonly textEmailErrorMessage = $('.form-error');
+
+  async login(email: string, password: string) {
+    // in some cases(1/10), protractor says that email and password he set but visually I can see that they are empty.
+    // Decided to wait for visibility of the field and then start to act
+    await this.waitForVisible(this.inputUserName);
+    await this.clearAndSetInputValue(this.inputUserName, email);
+    await this.clearAndSetInputValue(this.inputPassword, password);
+    await this.btnLogin.click();
+  }
 
   async login(email: string, password: string) {
     // in some cases(1/10), protractor says that email and password he set but visually I can see that they are empty.
--- e2e/src/tests/login.e2e-spec.ts
+++ e2e/src/tests/login.e2e-spec.ts
@@ -6,59 +6,41 @@ import { DashboardPo } from '../pages/dashbord.po';
 import { AccountDataMock } from '../data/account-data.mock';
 import { DataProvider } from '../data/data-provider';
 
-
-6. Что касательно подобной реализации:
-  `await profilePage.iconEditProfHeadline.click();
- await profilePage.setInputValue(profilePage.inputProfHeadline, accountData.professionalHeadline);`
-
-Нужно подобные клики, вводы в инпут филды и т.д. обернуть в методы в PO. Поскольку для второго теста тебе придется снова делать всю эту цепочку:
-  - создать экземпляр класса
-- вызвать веб элемент
-- вызвать метод для манипуляции с этим элеметом.
-
-  В результате, а) слишком длинная строка выходит, б) а если нужно будет ожидание добавить или какое-то специфический трюк сделать, это тоже в тесте будет?
-  По итогу, после переноса в пейдж обжекты будут красивые тесты
-  `await profilePage.clickEditProfileIcon();
- await profilePage.setProfileHeadline(stringValue);`
-c) ты не светишь вебэлементы в спеках. Для этого сделай их приватными внутри ПО.
-
-7. Что касается логин тестов.
-1) Для теста логин, где ввод в поле email и в поле password являются сами по себе шагами теста, нужно такие действия явно прописывать. Объединенный метод login уже лучше юзать для тех тестов, где это не является основным шагом из тест кейса, к пр. банально в прекондишине.
-2) Нужно добавить тест для негативного сценария, когда были введены неверные данные. Проверить ошибку валидации и что пользователь остался в форме логина.
-
-8. Когда создаешь отдельные объекты с датасетом и затем вытягиваешь значения в тесте, юзай деструктуризацию. Получается лаконично и читабельно, к пр.:
-`  it('check add education item', async () => {
-     const {education: {country, university, degree, startYear, endYear}} = accountData; // наша деструктуризация объекта
-     
-     ...
-     await profilePage.selectorCountry(country); // непосредственно уже передача значения в метод
-    ...
-   });`
-​
-9. В названии тестов лучше начинать со слова should, i.e "should be able to edit user profile" or 'should verify ...'
-
-
-
-
-
 describe('Login functionality', () => {
 
   const accountData = AccountDataMock;
 
+  const { email, password, userId, firstName, lastName } = accountData;
+
   const loginPage = new LoginPo();
   const dashboardPage = new DashboardPo();
 
-  it('check ability to login', async () => {
+  beforeAll(async () = > {
     await loginPage.open();
-    await this.waitForVisible(this.inputUserName);
-    await this.clearAndSetInputValue(this.inputUserName, accountData.email);
-    await this.clearAndSetInputValue(this.inputPassword, accountData.password);
-    await this.btnLogin.click();
+  })
+
+  it('check ability to login with CORRECT password and email', async () => {
+    await loginPage.login(email, password);
 
     expect(await browser.getCurrentUrl()).toContain(dashboardPage.url);
     expect(await dashboardPage.textWelcomeBack.getText()).toEqual(DataProvider.dashboardPage.welcomeText);
-    expect(await dashboardPage.textUserInitials.getText()).toEqual(formatUserName(accountData.firstName, accountData.lastName));
-    expect(await dashboardPage.textUserName.getText()).toEqual('@' + accountData.userId);
+    expect(await dashboardPage.textUserInitials.getText()).toEqual(formatUserName(firstName, lastName));
+    expect(await dashboardPage.textUserName.getText()).toEqual('@' + userId);
+  });
+
+  it('check ability to login with INCORRECT password', async () => {
+    await loginPage.login(email, 'incorrect_pass');
+
+    expect(await browser.getCurrentUrl()).toContain(loginPage.url);
+    const errorMessage = 'Incorrect username or password provided.';
+    expect(await loginPage.textPswrdErrorMessage.getText()).toEqual(errorMessage);
+  });
+
+  it('check ability to login with INCORRECT email', async () => {
+    await loginPage.login('incorrect_email', password);
+
+    expect(await browser.getCurrentUrl()).toContain(loginPage.url);
+    const errorMessage = 'Please enter a valid username or email address.';
+    expect(await loginPage.textPswrdErrorMessage.getText()).toEqual(errorMessage);
   });
 });
-
